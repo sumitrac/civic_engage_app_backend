95c0863d0d5965666a8e9d5c27a2ddb55b007fce
Sumitra <sumitrac2008@gmail.com>
2021-07-29 16:22:44 -0700

    clean up cred

--- event.py
+++ event.py
@@ -6,8 +6,10 @@ import firebase_admin
 from firebase_admin import credentials 
 from firebase_admin import firestore 
 
-html_text = requests.get('https://www.portland.gov/events')
-soup = BeautifulSoup(html_text, 'html.parser')    #To use lxml, need to instatl pip install lxml 
+# Getting data from external site using BeautifulSoup 
+html_text = requests.get('https://www.portland.gov/events').text
+soup = BeautifulSoup(html_text, 'html.parser')    
+#To use lxml, need to install pip install lxml 
 
 # events_title = soup.find_all('h2', class_="h3 order-1 mb-2").text
 # events_title.text
@@ -20,26 +22,28 @@ event_body = soup.find('div', class_="row no-gutters").text
 # event_date = soup.find('span', class_="d-flex allign-items-center")
 
 # event = soup.find('span', class_ = ("field field--name--title field--type--string field--label-hidden") 
-
 print(event_tag)
 print(event_title)
 print(event_body)
 
 
-# import firebase_admin
-# from firebase_admin import credentials 
-# from firebase_admin import firestore 
-
-cred = credentials.Certificate("civic-engage-capstone-firebase-adminsdk-hkf3r-b65348f762.json")
+# Sending data to firestore 
+cred = credentials.Certificate("civic-engage-capstone-firebase-adminsdk-hkf3r-17f4b9b0b2.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def save(collection_id, event_tag, event_title, event_body):
-    db.collection(collection_id).document(event_tag).document(event_title).data(event_body)
+def save(collection_id, document_id, event_tag, event_title, event_body):
+    # help(db.collection(collection_id).document(event_tag))
+    # db.collection(collection_id).document(event_tag).set({"title": event_title, "body": event_body})
+    db.collection(collection_id).document(document_id).set({"tag": event_tag, "title": event_title, "body": event_body})
+
+    # db.collection(collection_id).document(event_tag).data(event_title).data(event_body)
 
 save(
     collection_id = "event_1",
+    document_id= "document_id",
     event_tag = "event_tag",
     event_title = "event_title",
     event_body = "event_body"
 )
+
